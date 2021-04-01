721991301a40e511e6698d43c1d19692856639fa
Shannon Coyle <s1226097@monmouth.edu>
2021-03-31 20:53:04 -0400

    Refactoring index.js so it references the controller classes made

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const path = require('path')
+//const path = require('path')
 
 const app = new express()
 const ejs = require('ejs')
@@ -15,7 +15,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
  
-const BlogPost = require('./models/BlogPost.js')
+//const BlogPost = require('./models/BlogPost.js')
 app.use(express.static('public'))
 
 // Request handler for '/posts/store'
@@ -37,46 +37,26 @@ const validateMiddleWare = (req,res,next)=>{
 }
 app.use('/posts/store',validateMiddleWare)
 
-// Move existing request handler from newPost.js
+// Import controllers 
 const newPostController = require('./controllers/newPost')
+const homeController = require('./controllers/home')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
 
 app.listen(4000, ()=>{
     console.log("App listening on port 4000")
 })
 
-app.get('/', async (req,res)=>{
-   //res.sendFile(path.resolve(__dirname, 'index.html'))
-   const blogposts = await BlogPost.find({})
-   res.render('index', {
-       blogposts
-   }); 
-})
+app.get('/', homeController)
 
-app.get('/post/:id', async (req,res)=>{
-    const blogpost = await BlogPost.findById(req.params.id)
-    res.render('post', {
-        blogpost
-    })
- })
+app.get('/post/:id', getPostController)
 
- /*
+/*
 app.get('/posts/new', (req, res)=>{
   // Model creates a new doc with browser data
     res.render('create'); 
 }) */
+
 app.get('/posts/new', newPostController)
 
-app.post('/posts/store', async (req, res)=>{
-    let image = req.files.image; 
-
-    // Moves file elsewhere on server and name
-    image.mv(path.resolve(__dirname, 'public/img', image.name),
-    async(error) => {
-    // Model creates a new doc with browser data
-      await BlogPost.create({
-        ...req.body,
-        image: '/img/' + image.name
-      })
-      res.redirect('/')
-    })
-})
+app.post('/posts/store', storePostController)
