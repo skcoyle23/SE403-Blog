19f1cb3a2d37ff670569b73cbca9256b79fa89fa
ethan <s1174600@monmouth.edu>
2021-04-12 11:37:44 -0400

    Changed storeUser.js again to display the error notification in the page itself

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -3,7 +3,9 @@ const path = require('path')
 
 module.exports = (req, res) =>{
     User.create(req.body, (error, user) =>{
-        console.log(error)
+        if(error){
+            return res.redirect('/auth/register')
+        }
         res.redirect('/')
     })
 }
