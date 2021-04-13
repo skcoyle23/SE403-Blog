0e139dd3cf9d7138ca346a6ff97038688ee126a2
Shannon Coyle <s1226097@monmouth.edu>
2021-04-12 21:30:19 -0400

    Editing code to check if session contains user id

--- controllers/newPost.js
+++ controllers/newPost.js
@@ -3,5 +3,8 @@
 * from the user to create a new blog post
 */
 module.exports = (req, res) =>{
-    res.render("create")
+    if(req.session.userId) {
+        return res.render("create"); 
+    }
+    res.redirect('/auth/login')
 }
