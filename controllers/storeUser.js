0ff3ce9fecc58f92fb2c2cf3851e61dff7139327
Shannon Coyle <s1226097@monmouth.edu>
2021-04-18 13:46:23 -0400

    Assigning error messages to a variable

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -4,7 +4,8 @@ const path = require('path')
 module.exports = (req, res) =>{
     User.create(req.body, (error, user) =>{
         if(error){
-            Object.keys(error.errors).map(key => error.errors[key].message)
+            const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
+            req.session.validationErrors = validationErrors
             return res.redirect('/auth/register')
         }
         res.redirect('/')
