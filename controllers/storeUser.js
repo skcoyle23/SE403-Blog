476cc275f95d66337b4275698fd6e47fee466866
Shannon Coyle <s1226097@monmouth.edu>
2021-04-18 13:42:16 -0400

    Formatting the errors in storeUser.js

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -4,6 +4,7 @@ const path = require('path')
 module.exports = (req, res) =>{
     User.create(req.body, (error, user) =>{
         if(error){
+            Object.keys(error.errors).map(key => error.errors[key].message)
             return res.redirect('/auth/register')
         }
         res.redirect('/')
