658f1ade1a3821b19081ade6d9e9fddc960ae219
ethan <s1174600@monmouth.edu>
2021-04-21 00:14:26 -0400

    Added a unique validator to check for duplicate entries and report them

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema; 
+const Schema = mongoose.Schema;
+var uniqueValidator = require('mongoose-unique-validator');
+
 const bcrypt = require('bcrypt')
 
 const UserSchema = new Schema ({
@@ -13,6 +15,8 @@ const UserSchema = new Schema ({
         required: true
     }
 });
+
+UserSchema.plugin(uniqueValidator);
 
 /*
 * Allows us to change user data before saving it into the database
