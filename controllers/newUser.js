1568acfa60c738416eb4a987650ac28d5c30bb42
Shannon Coyle <s1226097@monmouth.edu>
2021-04-25 13:51:01 -0400

    More editing to controllers classes, User.js and register.ejs

--- controllers/newUser.js
+++ controllers/newUser.js
@@ -1,13 +1,12 @@
 module.exports = (req, res) =>{
     var username = ""
     var password = ""
-    const data = req.flash('data')[0];
+    const data = req.flash('data')[0]; 
 
-    if(typeof data != "undefined"){
+    if(typeof data != "undefined") {
         username = data.username
         password = data.password
     }
-
 
     res.render('register', { // render register.ejs
         //errors: req.session.validationErrors
